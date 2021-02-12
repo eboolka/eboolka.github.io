0dbdc8ce7699a1b2a3719cd1b31b3005891b8ccf
din <78966637+eboolka@users.noreply.github.com>
2021-02-12 19:50:22 +0300

    Add files via upload

---
+++ work326/files/script.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+    if(window.innerWidth > 1000) {
+        var i = 0;
+
+        function yved() {
+            i = 1;
+            $('.yved:nth-child(' + i + ')').fadeIn(500).delay(7000).fadeOut(500);
+        }
+
+        setTimeout(function () {
+            setInterval(
+                function () {
+                    i = i + 1;
+                    $('.yved:nth-child(' + i + ')').fadeIn(500).delay(7000).fadeOut(500);
+                }, 50000);
+            yved();
+        }, 20000);
+    }
+});
